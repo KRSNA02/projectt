8af1d665b23b51a9d1ad31f1b9f9d82bc14f325f
shanmukhasri <40070267+shanmukhasrisiva@users.noreply.github.com>
2018-07-12 21:15:07 +0530

    delbutadded
    delbutadded

--- src/pages/timesheet/timesheet.ts
+++ src/pages/timesheet/timesheet.ts
@@ -17,7 +17,7 @@ import { ToastController } from 'ionic-angular';
 
 export class TimesheetPage {
   
-color: string;
+color:Date;
   constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController, private toastCtrl: ToastController) {
     this.color=navParams.get('data');
   }
@@ -25,22 +25,47 @@ color: string;
 public des=[];
 public hours=[];
 public selctval='';
-public selctime='';
+public selctime:number;
+public selcthr='';
+public selctmin='';
 public works=["Health Check","Others","Meeting","OptIN"];
 public times=[1,2,3,4,5,6,7,8];
+public i:number;
+public a:number;
+public delw: string;
+public j:number;
 
 add = 0;
 
   additem()
   {
+      this.j= parseInt(this.selctmin)/60
+      this.selctime= parseInt(this.selcthr)+this.j;
       this.des.push(this.selctval);
-      this.hours.push(parseInt(this.selctime));
-      this.add = this.add + parseInt(this.selctime);
+      this.hours.push(this.selctime);
+      this.add = this.add + this.selctime;
       
       
               
           }
         
+          removeItem(delh: any){
+          this.a =this.hours.indexOf(delh);
+          this.delw=this.des[this.a];
+          if(this.a!=-1)
+          {
+            this.des.splice(this.a,1);
+          }
+          if(this.a!=-1)
+          {
+            this.hours.splice(this.a,1);
+          }
+          this.add=this.add-delh;
+
+
+
+
+          }
 save()
 {
 
