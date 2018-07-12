24cf9e0a9226393ed69435f3f05da9a9490c9657
KRSNA02 <pvbalu.india@gmail.com>
2018-07-12 10:36:09 +0530

    first commit

--- src/pages/viewhistory/viewhistory.ts
+++ src/pages/viewhistory/viewhistory.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the HomePage page.
+ * Generated class for the ViewhistoryPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,16 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
+  selector: 'page-viewhistory',
+  templateUrl: 'viewhistory.html',
 })
-export class HomePage {
+export class ViewhistoryPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+    console.log('ionViewDidLoad ViewhistoryPage');
   }
 
 }
